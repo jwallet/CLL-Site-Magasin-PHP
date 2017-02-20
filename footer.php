dfdd3de4cddcea504bf54e66d2246b512f67dba4
Guillaume Prud'homme <guillaumeprudhomme12@gmail.com>
2017-02-20 13:22:34 -0500

    Add footer

--- footer.php
+++ footer.php
@@ -1,28 +1,11 @@
 <footer class="page-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-                <!--<h5 class="white-text">Footer Content</h5>-->
-                <tr>
-                    <td><a class="grey-text text-lighten-3" href="#!">Nous joindre  |  </a></td>
-                    <td><a class="grey-text text-lighten-3" href="#!">Confidentialité</a></td>
-                </tr>
-            </div>
-            <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                    <li><i class="material-icons md-24">face</i></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
     <div class="footer-copyright">
         <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            © <?php echo date("Y"); ?> Copyright
+            <div class="right">
+                <a class="grey-text text-lighten-4" href="#!">Nous joindre</a> |
+                <a class="grey-text text-lighten-4" href="#!">Confidentialité</a>
+            </div>
         </div>
     </div>
 </footer>

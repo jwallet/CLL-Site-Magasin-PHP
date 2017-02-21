293d97ed26459f047782f687cdb4b9f2fc8f989b
Guillaume Prud'homme <guillaumeprudhomme12@gmail.com>
2017-02-21 09:23:32 -0500

    Init Menu Admin

--- contenu.php
+++ contenu.php
@@ -4,8 +4,19 @@ switch ($page[count($page)-1]){
     case 'accueil.php';
         include_once("accueil-contenu.php");
         break;
-
     case 'admin.php';
         include_once("admin-contenu.php");
+        break;
+    case 'admin-majclient.php';
+        include_once("admin-majclient-contenu.php");
+        break;
+    case 'admin-formcommande.php';
+        include_once("admin-formcommande-contenu.php");
+        break;
+    case 'admin-menu.php';
+        include_once("admin-menu-contenu.php");
+        break;
+    case 'admin-majcommande.php';
+        include_once("admin-majcommande-contenu.php");
         break;
 }?>
--- template.php
+++ template.php
@@ -1,16 +1,16 @@
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <?php include_once("meta.php"); ?>
-        <link rel="stylesheet" type="text/css" href="css/materialize.css">
-        <script src="js/materialize.js"></script>
-    </head>
-    <body>
-        <?php
-            include_once("header.php");
-            include_once("menu.php");
-            include_once("contenu.php");
-            include_once("footer.php");
-        ?>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <?php include_once("meta.php"); ?>
+    <link rel="stylesheet" type="text/css" href="css/materialize.css">
+    <script src="js/materialize.js"></script>
+</head>
+<body>
+<?php
+include_once("header.php");
+include_once("menu.php");
+include_once("contenu.php");
+include_once("footer.php");
+?>
+</body>
 </html>

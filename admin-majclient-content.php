a099546eff491860f4cbfeb7853b0acc7c7c1032
Guillaume Prud'homme <guillaumeprudhomme12@gmail.com>
2017-02-21 10:12:48 -0500

    Init MajClient

--- admin-majclient-content.php
+++ admin-majclient-content.php
@@ -1,41 +1,31 @@
 <div class="row">
-    <form class="col s12">
+    <form class="col s12" method="post" action="#">
         <div class="row">
             <div class="input-field col s6">
-                <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                <label for="first_name">First Name</label>
+                <i class="material-icons prefix">account_circle</i>
+                <input id="icon_prenom" type="text" class="validate">
+                <label for="icon_prenom">Prénom</label>
             </div>
             <div class="input-field col s6">
-                <input id="last_name" type="text" class="validate">
-                <label for="last_name">Last Name</label>
+                <i class="material-icons prefix">account_circle</i>
+                <input id="icon_Nom" type="text" class="validate">
+                <label for="icon_Nom">Nom</label>
             </div>
         </div>
         <div class="row">
-            <div class="input-field col s12">
-                <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-                <label for="disabled">Disabled</label>
+            <div class="input-field col s6">
+                <i class="material-icons prefix">phone</i>
+                <input id="icon_telephone" type="tel" class="validate">
+                <label for="icon_telephone">Telephone</label>
+            </div>
+            <div class="input-field col s6">
+                <i class="material-icons prefix">email</i>
+                <input id="icon_email" type="tel" class="validate">
+                <label for="icon_email">Email</label>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="password" type="password" class="validate">
-                <label for="password">Password</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="email" type="email" class="validate">
-                <label for="email">Email</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col s12">
-                This is an inline input field:
-                <div class="input-field inline">
-                    <input id="email" type="email" class="validate">
-                    <label for="email" data-error="wrong" data-success="right">Email</label>
-                </div>
-            </div>
-        </div>
+        <button class="btn waves-effect waves-light right" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+        </button>
     </form>
 </div>

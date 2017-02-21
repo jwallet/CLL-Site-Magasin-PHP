3f241a7eac5ed921d8eefe33ea2368f036756e75
Guillaume Prud'homme <guillaumeprudhomme12@gmail.com>
2017-02-21 09:40:53 -0500

    Init Admin

--- admin-contenu.php
+++ admin-contenu.php
@@ -1,7 +1,6 @@
-
 <div class="collection">
-    <a class="collection-item center-align" href="admin-majclient-contenu.php"><i class="medium material-icons">person</i></a>
-    <a class="collection-item center-align" href="admin-majclient-contenu.php"><i class="medium material-icons">shopping_basket</i></a>
-    <a class="collection-item center-align" href="admin-majclient-contenu.php"><i class="medium material-icons">restaurant</i></a>
-    <a class="collection-item center-align" href="admin-majclient-contenu.php"><i class="medium material-icons">restaurant_menu</i></a>
+    <a class="collection-item center" href="admin-majclient-contenu.php"><i class="medium material-icons">person</i>Mise à Jour d'un client</a>
+    <a class="collection-item center" href="admin-majcommande-contenu.php"><i class="medium material-icons">shopping_basket</i>Commande des clients</a>
+    <a class="collection-item center" href="admin-menu-contenu.php"><i class="medium material-icons">restaurant_menu</i>Menu de la semaine</a>
+    <a class="collection-item center" href="admin-formcommande-contenu.php"><i class="medium material-icons">restaurant</i>Formulaire commandes clients</a>
 </div>

cce24134a67b433e7630a0e21ef66f5f2f7aa29c
Guillaume Prud'homme <guillaumeprudhomme12@gmail.com>
2017-02-22 19:23:51 -0500

    some stuff

--- admin-content.php
+++ admin-content.php
@@ -3,16 +3,18 @@
             <div class="background">
                 <img src="images/office.jpg">
             </div>
-<!--            <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>-->
-<!--            <a href="#!name"><span class="white-text name">John Doe</span></a>-->
-<!--            <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>-->
+            <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
+            <a href="#!name"><span class="white-text name">John Doe</span></a>
+            <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
         </div></li>
-    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-    <li><a href="#!">Second Link</a></li>
-    <li><div class="divider"></div></li>
-    <li><a class="subheader">Subheader</a></li>
+    <div class="collection">
+        <li><a class="collection-item center-align" href="admin-majclient.php"><i class="small material-icons">person</i>Mise à jour client</a></li>
+        <li><a class="collection-item center-align" href="admin-majclient.php"><i class="small material-icons">shopping_basket</i>Commandes Clients</a></li>
+        <li><a class="collection-item center-align" href="admin-majclient.php"><i class="small material-icons">restaurant</i>Menu de la semaine</a></li>
+        <li><a class="collection-item center-align" href="admin-majclient.php"><i class="small material-icons">restaurant_menu</i>Formulaire des commandes</a></li>
+    </div>
 </ul>
-<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+<a href="#" data-activates="slide-out" class="button-collapse show-on-small"><i class="material-icons">menu</i></a>
 <div class="collection">
     <a class="collection-item center-align" href="admin-majclient.php"><i class="medium material-icons">person</i>Mise à jour client</a>
     <a class="collection-item center-align" href="admin-majclient.php"><i class="medium material-icons">shopping_basket</i>Commandes Clients</a>
--- admin-majclient-content.php
+++ admin-majclient-content.php
@@ -1,24 +1,28 @@
 <div class="row">
     <form class="col s12" method="post" action="#">
         <div class="row">
-            <div class="input-field col s6">
+            <div class="input-field col s12">
                 <i class="material-icons prefix">account_circle</i>
                 <input id="icon_prenom" type="text" class="validate">
                 <label for="icon_prenom">Prénom</label>
             </div>
-            <div class="input-field col s6">
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
                 <i class="material-icons prefix">account_circle</i>
                 <input id="icon_Nom" type="text" class="validate">
                 <label for="icon_Nom">Nom</label>
             </div>
         </div>
         <div class="row">
-            <div class="input-field col s6">
+            <div class="input-field col s12">
                 <i class="material-icons prefix">phone</i>
                 <input id="icon_telephone" type="tel" class="validate">
                 <label for="icon_telephone">Telephone</label>
             </div>
-            <div class="input-field col s6">
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
                 <i class="material-icons prefix">email</i>
                 <input id="icon_email" type="tel" class="validate">
                 <label for="icon_email">Email</label>

11399823d6f357d6d5165b94cb8a12af1fab226c
Guillaume Prudhomme <guillaumeprudhomme12@gmail.com>
2017-02-23 10:37:48 -0500

    some stuff

--- admin-content.php
+++ admin-content.php
@@ -21,6 +21,7 @@
 <!--    <a class="collection-item center-align" href="admin-majclient.php"><i class="medium material-icons">restaurant</i>Menu de la semaine</a>-->
 <!--    <a class="collection-item center-align" href="admin-majclient.php"><i class="medium material-icons">restaurant_menu</i>Formulaire des commandes</a>-->
 <!--</div>-->
+
 <?php
 
 $DayOfWeek = date('N') - 1;
@@ -28,19 +29,31 @@ $WeekStart = new DateTime(date('Y-m-d'));
 $WeekStart->sub(new DateInterval('P' . $DayOfWeek . 'D'));
 $WeekEnd = new DateTime($WeekStart->format('Y-m-d'));
 $WeekEnd->add(new DateInterval('P6D'));
-echo $WeekStart=$WeekStart->format('Y-m-d');
-echo $WeekEnd = $WeekEnd->format('Y-m-d');
-echo $query = "SELECT * FROM menu WHERE datemenud between '$WeekStart' and '$WeekEnd';";
+$WeekStart=$WeekStart->format('Y-m-d');
+$WeekEnd = $WeekEnd->format('Y-m-d');
+//$query = "SELECT  FROM menu m INNER JOIN relmenunourr  mn on m.idmenu = mn.idmenu INNER JOIN nourriture n on n.idrepas = mn.idrepas WHERE datemenud between '$WeekStart' and '$WeekEnd';";
+$query = "SELECT n.nomrepas,n.description,n.prixportion FROM menu m,relmenunourr rmn,nourriture n WHERE datemenud between '$WeekStart' and '$WeekEnd' and rmn.idmenu = m.idmenu and n.idrepas = rmn.idrepas;";
 //$query = "UPDATE menu SET description='bonjour' WHERE idmenu='1';";
 $result = $mysqli->query($query) or die("Error: ".mysqli_error($mysqli));
 if (!$result) {
     printf("Error: %s\n", mysqli_error($mysqli));
     exit();
 }
+echo "<ul class=\"collection\">";
 while ($val = $result->fetch_assoc()){
-    echo "<br><h4> " . "id:  " . $val['idmenu'] . "</br>";
-    echo "<h4>" . "desc:  " . $val['description'] ;
+//    echo "<li class=\"collection-item dismissable\"><div>" . $val['nomrepas'] ."<a href=\"#!\" class=\"secondary-content\"><i class=\"material-icons\">send</i></a></div></li>";
+    echo "<li class=\"collection-item avatar\">
+            <img src=\"images/yuna.jpg\" alt=\"\" class=\"circle\">
+            <span class=\"title\">". $val['nomrepas'] ."</span>
+            <p> Description: " . $val['description'] . "<br>
+                Prix par portion: ". $val['prixportion']."$
+            </p>
+            <a href=\"#!\" class=\"secondary-content\"><i class=\"material-icons\">grade</i></a>
+        </li>";
+//    echo "<br><h4> " . "id:  " . $val['idmenu'] . "</br>";
+//    echo "<h4>" . "desc:  " . $val['description'] ;
 }
+echo "</ul>";
 //$val = mysqli_fetch_array($result);
 //$description = $val["description"];
 //echo $description;

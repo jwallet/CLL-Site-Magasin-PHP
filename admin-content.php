44d8852d7dc8d7b5ffba7de004026468cce907d7
Jose Ouellet <joseouellet@gmail.com>
2017-02-27 11:59:45 -0500

    menu

--- admin-content.php
+++ admin-content.php
@@ -1,3 +1,9 @@
+<script type="text/javascript">
+    $(document).ready(function () {
+        Materialize.toast('Le plat a été ajouté avec succès', 8000);
+    });
+</script>
+
 <ul class="collapsible" data-collapsible="accordion">
 
     <!-- Commandes -->
@@ -31,7 +37,7 @@
                 <li>
                     <div class="collapsible-header grey lighten-5">
                         <div class="container">
-                            <i class="material-icons"></i>Gérer les commandes
+                            <i class="material-icons"></i>Gérer une commande de client
                             <span class="secondary-content"><i class="material-icons <?php echo $_GLOBAL['couleur1'] ?>-text">keyboard_arrow_down</i></span>
                         </div>
                     </div>
@@ -71,139 +77,17 @@
         </div>
     </li>
 
-    <!-- calendrier -->
-    <li>
-        <div class="collapsible-header">
-            <div class="container">
-                <span class="secondary-content"><i class="material-icons <?php echo $_GLOBAL['couleur1'] ?>-text">keyboard_arrow_down</i></span>
-                <i class="material-icons">date_range</i>Calendrier des menus
-            </div>
-        </div>
-        <div class="collapsible-body" style="padding:0;">
-            <ul class="collapsible" data-collapsible="accordion">
-                <li>
-                    <a href="#" class="black-text">
-                        <div class="collapsible-header grey lighten-5">
-                            <div class="container">
-                                <i class="material-icons"></i>Définir le menu actuel
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="black-text">
-                        <div class="collapsible-header grey lighten-5">
-                            <div class="container">
-                                <i class="material-icons"></i>Définir le menu des semaines avenirs
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <div class="collapsible-header grey lighten-5">
-                        <div class="container">
-                            <i class="material-icons"></i>Définir les jours pour commander
-                            <span class="secondary-content"><i class="material-icons <?php echo $_GLOBAL['couleur1'] ?>-text">keyboard_arrow_down</i></span>
-                        </div>
-                    </div>
-                    <div class="collapsible-body" style="padding:0;">
-                        <ul class="collapsible" data-collapsible="accordion">
-                            <li>
-                                <a href="#" class="black-text">
-                                    <div class="collapsible-header grey lighten-3">
-                                        <div class="container">
-                                            <i class="material-icons"></i>Jour d'ouverture
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="black-text">
-                                    <div class="collapsible-header grey lighten-3">
-                                        <div class="container">
-                                            <i class="material-icons"></i>Jour de fermeture
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="black-text">
-                                    <div class="collapsible-header grey lighten-3">
-                                        <div class="container">
-                                            <i class="material-icons"></i>Jour d'envoie d'une alerte aux clients n'ayant pas commander
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </li>
-
 
     <!-- menus -->
     <!--permet de gerer le titre d'un menu et d'associer des plats a celui-ci.-->
     <li>
-        <div class="collapsible-header">
-            <div class="container">
-                <span class="secondary-content"><i class="material-icons <?php echo $_GLOBAL['couleur1'] ?>-text">keyboard_arrow_down</i></span>
-                <i class="material-icons">map</i>Menus
+        <a href="#" class="black-text">
+            <div class="collapsible-header">
+                <div class="container">
+                    <i class="material-icons">map</i>Menus
+                </div>
             </div>
-        </div>
-        <div class="collapsible-body" style="padding:0;">
-            <ul class="collapsible" data-collapsible="accordion">
-                <li>
-                    <a href="#" class="black-text">
-                        <div class="collapsible-header grey lighten-5">
-                            <div class="container">
-                                <i class="material-icons"></i>Lier des plats à un menu
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <div class="collapsible-header grey lighten-5">
-                        <div class="container">
-                            <i class="material-icons"></i>Gérer les menus
-                            <span class="secondary-content"><i class="material-icons <?php echo $_GLOBAL['couleur1'] ?>-text">keyboard_arrow_down</i></span>
-                        </div>
-                    </div>
-                    <div class="collapsible-body" style="padding:0;">
-                        <ul class="collapsible" data-collapsible="accordion">
-                            <li>
-                                <a href="#" class="black-text">
-                                    <div class="collapsible-header grey lighten-3">
-                                        <div class="container">
-                                            <i class="material-icons"></i>Créer un nouveau menu
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="black-text">
-                                    <div class="collapsible-header grey lighten-3">
-                                        <div class="container">
-                                            <i class="material-icons"></i>Modifier un menu
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="black-text">
-                                    <div class="collapsible-header grey lighten-3">
-                                        <div class="container">
-                                            <i class="material-icons"></i>Supprimer un menu
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </div>
+        </a>
     </li>
 
     <!-- Les plats / items -->
@@ -271,7 +155,7 @@
                     <a href="#" class="black-text">
                         <div class="collapsible-header grey lighten-5">
                             <div class="container">
-                                <i class="material-icons"></i>Ajouter un nouveau compte client
+                                <i class="material-icons"></i>Rechercher un compte client
                             </div>
                         </div>
                     </a>
@@ -280,7 +164,7 @@
                     <a href="#" class="black-text">
                         <div class="collapsible-header grey lighten-5">
                             <div class="container">
-                                <i class="material-icons"></i>Rechercher un compte client
+                                <i class="material-icons"></i>Ajouter un nouveau compte client
                             </div>
                         </div>
                     </a>
@@ -318,26 +202,54 @@
         </a>
     </li>
 
-    <!-- modifier le slider sur la page accueil -->
+    <!-- pages web -->
     <li>
-        <a href="#" class="black-text">
-            <div class="collapsible-header">
-                <div class="container">
-                    <i class="material-icons">slideshow</i>Modifier le slideshow d'accueil
-                </div>
+        <div class="collapsible-header">
+            <div class="container">
+                <span class="secondary-content"><i class="material-icons <?php echo $_GLOBAL['couleur1'] ?>-text">keyboard_arrow_down</i></span>
+                <i class="material-icons">http</i>Modifier pages d'infos
             </div>
-        </a>
-    </li>
-
-    <!-- modifier nous contacter -->
-    <li>
-        <a href="#" class="black-text">
-            <div class="collapsible-header">
-                <div class="container">
-                    <i class="material-icons">perm_phone_msg</i>Modifier 'Nous contacter'
-                </div>
-            </div>
-        </a>
+        </div>
+        <div class="collapsible-body" style="padding:0;">
+            <ul class="collapsible" data-collapsible="accordion">
+                <li>
+                    <a href="#" class="black-text">
+                        <div class="collapsible-header grey lighten-5">
+                            <div class="container">
+                                <i class="material-icons"></i>A propos de l'entreprise
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="black-text">
+                        <div class="collapsible-header grey lighten-5">
+                            <div class="container">
+                                <i class="material-icons"></i>Nous contacter
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="black-text">
+                        <div class="collapsible-header grey lighten-5">
+                            <div class="container">
+                                <i class="material-icons"></i>Fonctionnement (FAQ)
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="black-text">
+                        <div class="collapsible-header grey lighten-5">
+                            <div class="container">
+                                <i class="material-icons"></i>Termes et conditions
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </li>
 
     <!-- sortie panneau admin -->

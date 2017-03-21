6a7d3c2ec36fd31fcd321490b40b789ebeed4d2d
Jose Ouellet <joseouellet@gmail.com>
2017-03-20 21:26:56 -0400

    500

---
+++ 500-content.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="section">
+        <h5>Désolé, quelque chose s'est brisée.</h5>
+        <div style="width:100%;height:400px; width: auto;background-repeat:no-repeat;background-image:url('css/res/500.png');background-size:500px auto;background-position:center;"></div>
+    </div>
+</div>

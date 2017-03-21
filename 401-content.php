51bbabe073865d6ecf82b97881e640a11d1dfcb4
Jose Ouellet <joseouellet@gmail.com>
2017-03-21 08:25:07 -0400

    erreurs en html

--- 401-content.php
+++ 401-content.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="section">
-        <h5>Désolé, nous n'avons pu trouver cette page.</h5>
-        <div style="width:100%;height:300px; width: auto;background-repeat:no-repeat;background-image:url('css/res/404.png');background-size:500px auto;background-position:center;"></div>
+        <h5>Désolé, vous n'avez pas les droits d'accès à cette page.</h5>
+        <div style="width:100%;height:300px; width: auto;background-repeat:no-repeat;background-image:url('css/res/401.png');background-size:500px auto;background-position:center;"></div>
     </div>
 </div>

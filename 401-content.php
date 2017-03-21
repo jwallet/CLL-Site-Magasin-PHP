9eaabe475d5f696034793e4731a975a48552531b
Jose Ouellet <joseouellet@gmail.com>
2017-03-21 08:24:42 -0400

    erreurs en html

---
+++ 401-content.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="section">
+        <h5>Désolé, nous n'avons pu trouver cette page.</h5>
+        <div style="width:100%;height:300px; width: auto;background-repeat:no-repeat;background-image:url('css/res/404.png');background-size:500px auto;background-position:center;"></div>
+    </div>
+</div>
